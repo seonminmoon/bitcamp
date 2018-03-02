2b6004926b8f6f9ee21f2add51cf96a6c05ce3cc
seonminmoon <se0nmin@nav\er.com>
2018-03-02 17:53:01 +0900

    'bitcamp-java-project'

---
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -0,0 +1,8 @@
+package bitcamp.java106.pms; 
+//src.main.java 까지는 적으면 안됨, java 폴더 밑에 .java 파일 둘거라서
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
