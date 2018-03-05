17ba4fcad2fcd78ca48a37e0360695dc21d77850
seonminmoon <se0nmin@nav\er.com>
2018-03-05 19:35:06 +0900

    '0305'

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -1,8 +1,31 @@
 package bitcamp.java106.pms; 
 //src.main.java 까지는 적으면 안됨, java 폴더 밑에 .java 파일 둘거라서
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+
+      
+        Scanner sc = new Scanner(System.in);
+        System.out.print("팀명? ");
+        String teamName = sc.next();
+
+        System.out.print("설명? ");
+        String title = sc.next();
+
+        System.out.print("최대인원? ");
+        int num = sc.nextInt();
+
+        System.out.print("시작일? ");
+        String startDay = sc.next();
+
+        System.out.print("종료일? ");
+        String endDay = sc.next();
+
+        System.out.println("-------------------");
+        System.out.println("팀명: " + teamName);
+        System.out.println("설명: " + title);
+        System.out.println("최대인원: " + num);
+        System.out.println("일자: " + startDay + " ~ " + endDay);
     }
-}
+} 
