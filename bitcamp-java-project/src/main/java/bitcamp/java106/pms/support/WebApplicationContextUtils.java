8f05535705191d0876f2f132f8a19afab3767312
seonm <seonm@DESKTOP-4DKM85D>
2018-05-22 18:23:30 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/support/WebApplicationContextUtils.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/support/WebApplicationContextUtils.java
@@ -13,7 +13,7 @@ public class WebApplicationContextUtils {
     // 이를 모방하여 만들기 위해 다음과 같이 맵에서 보관하는 방식을 사용하였다.
     public static Map<ServletContext, ApplicationContext> containers = 
             new HashMap<>();
-    
+     
     // 웹 애플리케이션에서 사용할 IoC 컨테이너를 꺼내고 싶다면,
     // 파라미터에 웹 애플리케이션 정보를 담고 있는 ServletContext를 넘겨줘야 한다.
     public static ApplicationContext getWebApplicationContext(ServletContext ctx) {
