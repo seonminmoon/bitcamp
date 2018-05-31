f0f0507bfa8e97674e6d23199bbcb581bd344161
seonminmoon <seonminmoon@gmail.com>
2018-05-31 10:17:33 +0900

    '.'

---
+++ spring-ioc/src/main/java/bitcamp/java106/step12/ex1/Exam01.java
@@ -0,0 +1,29 @@
+// Spring과 Mybatis 연동
+package bitcamp.java106.step12.ex1;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bitcamp.java106.BeanUtils;
+import bitcamp.java106.step12.Board;
+
+public class Exam01 {
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java106/step12/ex1/application-context.xml");
+        
+        BeanUtils.printBeanNames(iocContainer);
+        
+        BoardDao boardDao = iocContainer.getBean(BoardDao.class);
+        List<Board> list = boardDao.selectList();
+        
+        for (Board board : list) {
+            System.out.printf("%d, %s, %s \n", 
+                    board.getNo(),
+                    board.getTitle(),
+                    board.getRegisteredDate());
+        }
+    }
+}
