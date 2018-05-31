b041fc92aeddd3cb13dd01bbd6a34425b1f59aae
seonminmoon <se0nmin@naver.com>
2018-05-31 12:24:23 +0900

    .

--- spring-ioc/src/main/java/bitcamp/java106/step12/ex3/Exam01.java
+++ spring-ioc/src/main/java/bitcamp/java106/step12/ex3/Exam01.java
@@ -1,5 +1,5 @@
-// Spring과 Mybatis 연동 : 설정이 아니라 객체를 통해 SqlSessionFactory 만들기
-package bitcamp.java106.step12.ex2;
+// Spring과 Mybatis 연동 : mybatis에서 제공해주는 spring 연동 라이브러리 사용하기
+package bitcamp.java106.step12.ex3;
 
 import java.util.List;
 
@@ -11,8 +11,17 @@ import bitcamp.java106.step12.Board;
 
 public class Exam01 {
     public static void main(String[] args) {
+        // 우리가 직접 스프링 IoC 컨테이너에서 사용할 
+        // SqlSessionFactory 빌더 클래스를 만들 필요가 없다.
+        // mybatis 팀에서 제공해주는 FactoryBean을 사용하자!
+        // 1) mvnrepository.com 에서 mybatis-spring 라이브러리 검색
+        // 2) "build.gradle"에 의존 라이브러리 정보 추가
+        // 3) "gradle eclipse" 실행
+        // 4) 이클립스 프로젝트 갱신
+        // 5) 이 라이브러리에서 제공하는 FactoryBean 사용!
+        
         ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-                "bitcamp/java106/step12/ex2/application-context.xml");
+                "bitcamp/java106/step12/ex3/application-context.xml");
         
         BeanUtils.printBeanNames(iocContainer);
         
