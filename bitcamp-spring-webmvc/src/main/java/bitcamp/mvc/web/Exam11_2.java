acce76fe310457e92b1bf41173a67e27dc677c23
seonminmoon <se0nmin@naver.com>
2018-07-20 14:38:48 +0900

    .

--- bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam11_2.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam11_2.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import net.coobird.thumbnailator.Thumbnails;
+
 @RestController 
 @RequestMapping("/exam11_2") 
 public class Exam11_2 {
@@ -22,7 +24,7 @@ public class Exam11_2 {
     @Autowired ServletContext sc;
 
     @PostMapping("upload01")
-    public Object upload01(
+    public Object upload01( // 파일 한개 받기
             MultipartFile files) {
         HashMap<String, Object> jsonData = new HashMap<>();
 
@@ -47,7 +49,7 @@ public class Exam11_2 {
     
 
     @PostMapping("upload02")
-    public Object upload02(
+    public Object upload02( // 파일 여러개받기
             MultipartFile[] files) {
         // 여러 개 파일을 한 번에 보내기 => 추천하지 않는다.
         // 한번에 보내다가 중간에 오류나면 전체가 다 날라가기 때문에 개별로 보내는 방법을 추천함.
@@ -104,6 +106,41 @@ public class Exam11_2 {
         }
         return returnData;
     }
+    
+    
+
+    @PostMapping("upload04")
+    public Object upload04( // 썸네일
+            MultipartFile files) {
+        HashMap<String, Object> jsonData = new HashMap<>();
+
+        String filesDir = sc.getRealPath("/files");  // 진짜 Real Path (사진이 저장되는 폴더명)
+
+        int i = 0;
+        // file에 대해 original File명과 새 파일을 구분하기 위해서
+        String filename = UUID.randomUUID().toString();
+        jsonData.put("filename", filename);
+        jsonData.put("filesize", files.getSize());
+        jsonData.put("originname", files.getOriginalFilename());
+        try {
+            File path = new File(filesDir + "/" + filename);
+            files.transferTo(path); //파일 저장하기
+            
+            // 썸네일 이미지 생성
+            
+            Thumbnails.of(path)
+                        .size(50, 50)
+                        .outputFormat("jpg")
+                        .toFile(path.getCanonicalFile() + "_50x50");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonData;
+    }
+    
+
+    
     
 }
 
